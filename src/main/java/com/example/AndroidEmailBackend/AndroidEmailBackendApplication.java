7c880341c5610bd2ad7442cb3a2be06c5086816a
Zoran Marcetic <zoranmarcetic@hotmail.com>
2019-05-07 17:01:20 +0200

    basic code

---
+++ src/main/java/com/example/AndroidEmailBackend/AndroidEmailBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.AndroidEmailBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidEmailBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndroidEmailBackendApplication.class, args);
+	}
+
+}
